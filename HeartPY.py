bae502475a058b7eb5f9f76a69e9be7330e80c58
imsaitam <matutedesanto@gmail.com>
2022-10-20 18:02:40 -0300

    Fix conditions labels

--- HeartPY.py
+++ HeartPY.py
@@ -1,108 +1,109 @@
+from cProfile import label
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
-from tkinter import messagebox
 import serial
 
 arduino = serial.Serial('COM4', 9600)
 
-class Proyecto(tk.Tk):#contiene los metodos de la ventanas
-    def __init__(self, window) : #inicializa el estado de un objeto
-        self.wind = window #almacena la vetana como parametro
-        self.wind.title('Proyecto')#titulo de la ventana
-        self.wind.geometry('600x400+375+50') #dimensiones de la ventana
-        self.wind.resizable(False, False) #elimina la opcion de maximizar
-        self.windtext = Label(self.wind, text='Calculadora de salud', font="Century_Gothic")#titulo
-        self.windtext.pack(ipady= 30)#posicion
-#se crea un contenedor
-        frame = LabelFrame(self.wind, text ='Ingrese sus datos', font="Century_Gothic") #nombre del contenedor
-        frame.pack(ipady= 20, ipadx = 40, anchor= tk.N) #posicion del contenedor
-#input edad
-        self.labeledad = Label(frame, text= 'Edad')#texto que aparece adelante del input 
-        self.labeledad.pack(ipady=15, ipadx = 0)#posicion del texto
-        self.edad = ttk.Combobox(frame, state='readonly') #declaramos combobo  
-        self.edad.pack(ipady=0.5, ipadx= 2)#posicion del combobox
-        self.edad['values'] = ('20 a 29',  '29 a 39', '39 a 49', '+50')#valores del combobox
-        self.edad.current(0) #valor predeterminado
-        self.edad.pack(ipady=  0, ipadx= 0) #posicion del input
+class Proyecto(tk.Tk):#Contains the methods of the window
+    def __init__(self, window) : #initializes the state of an object
+        self.wind = window #store the window as a parameter
+        self.wind.title('Proyecto')#title of the windows
+        self.wind.geometry('600x400+375+50') #window dimensions
+        self.wind.resizable(False, False) #remove the maximize option
+        self.windtext = Label(self.wind, text='Calculadora de salud', font="Century_Gothic")#title
+        self.windtext.pack(ipady= 30)#position
+#a container is created
+        frame = LabelFrame(self.wind, text ='Ingrese sus datos', font="Century_Gothic") #name of the container
+        frame.pack(ipady= 20, ipadx = 40, anchor= tk.N) #position of the container
+#input age
+        self.labeledad = Label(frame, text= 'Edad')#text that appears in front of the input
+        self.labeledad.pack(ipady=15, ipadx = 0)#position text
+        self.edad = ttk.Combobox(frame, state='readonly') #declare combobox
+        self.edad.pack(ipady=0.5, ipadx= 2)#position of the combobox
+        self.edad['values'] = ('20 a 29',  '29 a 39', '39 a 49', '+50')#values of the combobox
+        self.edad.current(0) #default value
+        self.edad.pack(ipady=  0, ipadx= 0) #position input
         self.edad.focus()
-#Combobox Genero
+#Combobox Gender
         
-        self.labelgenero = Label(frame, text = 'Genero')  #texto que aparece en frente del combobox
-        self.labelgenero.pack(ipady=  15, ipadx= 0) #posicion del texto
-        self.genero = ttk.Combobox(frame, state='readonly') #declaramos combobo  
-        self.genero.pack(ipady=0.5, ipadx= 2)#posicion del combobox
-        self.genero['values'] = ('masculino',  'femenino')#valores del combobox
-        self.genero.current(0) #valor predeterminado
-#boton envio de datos
+        self.labelgenero = Label(frame, text = 'Genero')  #text that appears in front of the input
+        self.labelgenero.pack(ipady=  15, ipadx= 0) #position text
+        self.genero = ttk.Combobox(frame, state='readonly') #declare combobox
+        self.genero.pack(ipady=0.5, ipadx= 2)#position of the combobox
+        self.genero['values'] = ('masculino',  'femenino')#values of the combobox
+        self.genero.current(0) #default value
+#send data button
         def obtener_info():
-                genero = self.genero.get()#guarda las variables del campo genero
-                edad = self.edad.get()#guarda las variables del campo edad
-                bpm = int(arduino.readline().decode('utf-8')) #Comunicacion con Monitor serial
-                NewWind = Toplevel(window) #abre una ventana hija
-                NewWind.geometry('500x500')#configuracion de la ventana hija
-                NewWind.title('Coso')#titulo de la ventana hija
+                genero = self.genero.get()#save the variables of the gender field
+                edad = self.edad.get()#save the variables of the age field
+                bpm = int(arduino.readline().decode('utf-8')) #comunication with arduino monitor serial
+                NewWind = Toplevel(window) #open a new window
+                NewWind.geometry('500x500')#new window dimensions
+                NewWind.title('Coso')#title new window
                 self.wind.withdraw()
                 NewWind.update()
                 while True:
-                        bpm = int(arduino.readline().decode('utf-8')) #Comunicacion con Monitor serial
-                        Label(NewWind, text=bpm, font="Century_Gothic").place(x=0, y=0) #BPM en ventana Tkinter
-                        print(bpm)
-                        NewWind.update() #Actualización de la ventana
-                        if genero == "femenino":
-                                Label(NewWind, text="Su estado de salud es", font="Century_Gothic").place(x=100, y=100)
-                                if edad == "20 a 29":
-                                        if bpm >= 78 and bpm <= 94:
-                                                Label(NewWind, text="optima", font="Century_Gothic").place(x=100, y=200)
-                                        elif bpm < 78 or bpm > 94:
-                                                Label(NewWind, text="no optima", font="Century_Gothic").place(x=100, y=300)               
-                                elif edad == "29 a 39":
-                                        if bpm >= 80 and bpm <= 96:
-                                            Label(NewWind, text="optima", font="Century_Gothic").place(x=100, y=200)  
-                                        elif bpm < 80 and bpm > 96:
-                                                Label(NewWind, text="no optima", font="Century_Gothic").place(x=100, y=300)  
-                                elif edad == "39 a 49":
-                                        if bpm >= 80 and bpm <= 98:
-                                               Label(NewWind, text="optima", font="Century_Gothic").place(x=100, y=200)     
-                                        elif bpm > 80 and bpm < 98:  
-                                                Label(NewWind, text="no optima", font="Century_Gothic").place(x=100, y=300)          
-                                elif edad == "+50":
-                                        if bpm >= 84 and bpm <= 102:
-                                               Label(NewWind, text="optima", font="Century_Gothic").place(x=100, y=200) 
-                                        elif bpm > 84 and bpm < 102:
-                                                Label(NewWind, text="no optima", font="Century_Gothic").place(x=100, y=300)    
-                                
-                        if genero == "masculino":
-                                Label(NewWind, text="Su estado de salud es", font="Century_Gothic").place(x=100, y=100)
-                                if edad == "20 a 29":
-                                        if bpm >= 70 and bpm <= 84:
-                                                        Label(NewWind, text="optima", font="Century_Gothic").place(x=100, y=200)
-                                        elif bpm < 74 or bpm > 84:
-                                                Label(NewWind, text="no optima", font="Century_Gothic").place(x=100, y=300)               
-                                elif edad == "29 a 39":
-                                        if bpm >= 74 and bpm <= 84:
-                                            Label(NewWind, text="optima", font="Century_Gothic").place(x=100, y=200)  
-                                        elif bpm < 74 and bpm > 84:
-                                                Label(NewWind, text="no optima", font="Century_Gothic").place(x=100, y=300)  
-                                elif edad == "39 a 49":
-                                        if bpm >= 74 and bpm <= 88:
-                                               Label(NewWind, text="optima", font="Century_Gothic").place(x=100, y=200)     
-                                        elif bpm > 74 and bpm < 88:  
-                                                Label(NewWind, text="no optima", font="Century_Gothic").place(x=100, y=300)          
-                                elif edad == "+50":
-                                        if bpm >= 76 and bpm <= 88:
-                                               Label(NewWind, text="optima", font="Century_Gothic").place(x=100, y=200) 
-                                        elif bpm > 76 and bpm < 88:
-                                                Label(NewWind, text="no optima", font="Century_Gothic").place(x=100, y=300) 
+                                bpm = int(arduino.readline().decode('utf-8')) #comunication with arduino monitor serial
+                                Label(NewWind, text=bpm, font="Century_Gothic").place(x=0, y=0) #BPM in tkinter window
+                                print(bpm)
+                                NewWind.update() #window update
+                                if genero == "femenino":
+                                        label_estado = Label(NewWind, text="Su estado de salud es ", font="Century_Gothic")
+                                        label_estado.place(x=100, y=100)
+                                        if edad == "20 a 29":
+                                                if bpm >= 78 and bpm <= 94:
+                                                        label_estado.configure(text="Su estado de salud es optimo")
+                                                elif bpm < 78 or bpm > 94:
+                                                        label_estado.configure(text="Su estado de salud es no optimo")              
+                                        elif edad == "29 a 39":
+                                                if bpm >= 80 and bpm <= 96:
+                                                    label_estado.configure(text="Su estado de salud es optimo")
+                                                elif bpm < 80 and bpm > 96:
+                                                        label_estado.configure(text="Su estado de salud es no optimo") 
+                                        elif edad == "39 a 49":
+                                                if bpm >= 80 and bpm <= 98:
+                                                       label_estado.configure(text="Su estado de salud es optimo")    
+                                                elif bpm > 80 and bpm < 98:  
+                                                        label_estado.configure(text="Su estado de salud es no optimo")          
+                                        elif edad == "+50":
+                                                if bpm >= 84 and bpm <= 102:
+                                                       label_estado.configure(text="Su estado de salud es optimo")
+                                                elif bpm > 84 and bpm < 102:
+                                                        label_estado.configure(text="Su estado de salud es no optimo")    
+
+                                if genero == "masculino":
+                                        label_estado = Label(NewWind, text="Su estado de salud es ", font="Century_Gothic")
+                                        if edad == "20 a 29":
+                                                if bpm >= 70 and bpm <= 84:
+                                                        label_estado.configure(text="Su estado de salud es optimo")
+                                                elif bpm < 74 or bpm > 84:
+                                                        label_estado.configure(text="Su estado de salud es no optimo")          
+                                        elif edad == "29 a 39":
+                                                if bpm >= 74 and bpm <= 84:
+                                                    label_estado.configure(text="Su estado de salud es optimo") 
+                                                elif bpm < 74 and bpm > 84:
+                                                        label_estado.configure(text="Su estado de salud es no optimo")    
+                                        elif edad == "39 a 49":
+                                                if bpm >= 74 and bpm <= 88:
+                                                       label_estado.configure(text="Su estado de salud es optimo")  
+                                                elif bpm > 74 and bpm < 88:  
+                                                        label_estado.configure(text="Su estado de salud es no optimo")          
+                                        elif edad == "+50":
+                                                if bpm >= 76 and bpm <= 88:
+                                                       label_estado.configure(text="Su estado de salud es optimo")
+                                                elif bpm > 76 and bpm < 88:
+                                                        label_estado.configure(text="Su estado de salud es no optimo")   
                         
                 
                                    
                                         
-        self.boton = Button(frame, text = 'Enviar Datos', command=obtener_info).pack(ipady =0, ipadx= 36, pady=30) #creamos el botón que envia los datos
+        self.boton = Button(frame, text = 'Enviar Datos', command=obtener_info).pack(ipady =0, ipadx= 36, pady=30) #create the button that sends the data
                 
 
 
 if __name__  == '__main__' or 'NewWind':#comprueba si es el archivo main
-    window = Tk() #ventana principal
-    aplication = Proyecto(window) #se guarda la clase en una variable
-    window.mainloop() #inicializa la ventana
+    window = Tk() 
+    aplication = Proyecto(window) 
+    window.mainloop() 
